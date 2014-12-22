6a2b5d370b81a645304909c75863d6afabd22b87
Thomas Abeel <thomas@abeel.be>
2014-12-22 13:47:13 +0100

    improved CLI app with automatic documentation and app linking

--- scala/abeel/genometools/GenomeToolsConsole.scala
+++ scala/abeel/genometools/GenomeToolsConsole.scala
@@ -8,31 +8,32 @@ import abeel.genometools.gff2gtf.GFF2GTF
 import abeel.genometools.inject.InjectColumns
 import abeel.genometools.reducevcf.ReduceVCF
 import abeel.genometools.vcfstats.VCFStatistics
+import atk.util.Tool
 
-trait Main extends Object{
-  def main(args: Array[String]){}
+trait Main extends Tool {
+  def main(args: Array[String]) {}
 }
 object GenomeToolsConsole {
+
+  val instructions: Map[String, Main] = Map(
+
+    "bam2fraglendistr" -> Bam2FragmentlenDistribution,
+
+    "bam2readnames" -> Bam2ReadNames,
+    "bam2tdf" -> ConvertBAM2TDF,
+    "gbk2gff" -> GBK2GFF,
+    "gff2gtf" -> GFF2GTF,
+    "inject" -> InjectColumns,
+    "reducevcf" -> ReduceVCF,
+    "vcfstats" -> VCFStatistics)
   def main(args: Array[String]): Unit = {
-
-    val instructions:Map[String,Main] = Map(
-
-      "bam2fraglendistr" -> Bam2FragmentlenDistribution,
-
-      "bam2readnames" -> Bam2ReadNames,
-      "bam2tdf" -> ConvertBAM2TDF,
-      "gbk2gff" -> GBK2GFF,
-      "gff2gtf" -> GFF2GTF,
-      "inject" -> InjectColumns,
-      "reducevcf" -> ReduceVCF,
-      "vcfstats" -> VCFStatistics)
 
     if (args.length == 0 || !instructions.contains(args(0))) {
 
       listInstructions
     } else {
       val name = args(0)
-      val obj:Main = instructions(name)
+      val obj: Main = instructions(name)
       obj.main(args.drop(1))
 
     }
@@ -40,9 +41,10 @@ object GenomeToolsConsole {
   }
 
   def listInstructions() {
-    println("Usage:java -jar tcm.jar [instruction] [instruction options...]")
+    println("Usage:java -jar genometools.jar <instruction> [options...]")
     println("Instructions:")
-    
+    println(instructions.toList.sortBy(_._1).map(f => String.format("    %1$-20s",f._1) + f._2.description).mkString("\n"))
+
   }
 
 }
