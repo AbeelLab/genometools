5d1e709220f8ef1100b31c2f071c91b1a7a988cb
Thomas Abeel <thomas@abeel.be>
2018-07-19 12:14:40 -0400

    ++formatting

--- scala/pelican/ExtractDuplications.scala
+++ scala/pelican/ExtractDuplications.scala
@@ -9,16 +9,14 @@ object ExtractDuplications extends Main {
 
   case class Config(val input: File = null, val output: File = null)
 
-  /*
-   * /gsap/assembly_repository needs to be mapped to x:
-   */
- override def main(args: Array[String]): Unit = {
+  override def description = """Extract information about large duplications from Pilon runs"""
+  override def main(args: Array[String]): Unit = {
 
-    val parser = new scopt.OptionParser[Config]("java -jar pelican.jar extract-duplications") {
+    val parser = new scopt.OptionParser[Config]("java -jar genometools.jar extract-duplications") {
       opt[File]('i', "input") required () action { (x, c) => c.copy(input = x) } text ("Input file with ale identifiers and pilon file locations, i.e. ale.id.txt")
       opt[File]('o', "output") action { (x, c) => c.copy(output = x) } text ("Output file. Default input parent directory + duplications.txt")
 
-    } 
+    }
 
     parser.parse(args, Config()).map { config =>
       extract(config);
@@ -32,10 +30,8 @@ object ExtractDuplications extends Main {
       println("Processing: " + f)
       f -> tLines(f).filter(_.startsWith("Large collapsed region"))
     })
-    //    val oFolder=new File(folder+"/duplications/")
-    //    if(!oFolder.exists())
-    //      oFolder.mkdirs()
-    val pw = new PrintWriter(if(config.output!=null)config.output else new File(config.input.getAbsoluteFile().getParent()+"/duplications.txt"))
+
+    val pw = new PrintWriter(if (config.output != null) config.output else new File(config.input.getAbsoluteFile().getParent() + "/duplications.txt"))
     pw.println(generatorInfo)
     pw.println(mapping.map(f => f._1 + "\t" + f._2.mkString(",")).mkString("\n"))
     pw.close
