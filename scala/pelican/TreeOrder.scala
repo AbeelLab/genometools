3f4db77e498cb9166ea6eb0b089a58a2f2247813
Thomas Abeel <thomas@abeel.be>
2018-07-19 12:07:57 -0400

    ++formatting

--- scala/pelican/TreeOrder.scala
+++ scala/pelican/TreeOrder.scala
@@ -5,12 +5,14 @@ import scala.collection.JavaConversions._
 import java.io.File
 import abeel.genometools.Main
 
-object TreeOrder extends Main{
+object TreeOrder extends Main {
 
   case class Config(val tree: File = null)
-
- override def main(args: Array[String]): Unit = {
-    val parser = new scopt.OptionParser[Config]("java -jar pelican.jar list-tree") {
+  
+  override def description = """List all nodes in the tree in the order they will appear in the figure."""
+  
+  override def main(args: Array[String]): Unit = {
+    val parser = new scopt.OptionParser[Config]("java -jar genometools.jar list-tree") {
       opt[File]('t', "tree") required () action { (x, c) => c.copy(tree = x) } text ("Phylogenetic tree file.")
 
     }
