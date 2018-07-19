6c82876a52d53ac6dce44b048bb351f9887f6291
Thomas Abeel <thomas@abeel.be>
2018-07-19 12:10:18 -0400

    ++formatting

--- scala/pelican/PairwiseSNPdistance.scala
+++ scala/pelican/PairwiseSNPdistance.scala
@@ -10,10 +10,12 @@ import atk.util.Tool
 import atk.util.TimeInterval
 import abeel.genometools.Main
 
-object PairwiseSNPdistance  extends Main{
-
+object PairwiseSNPdistance extends Main {
+  
+  override def description = """Create pairwise SNP distance matrix"""
+  
   case class Config(output: File = new File("snp.matrix.txt"), input: File = null)
- override def main(args: Array[String]): Unit = {
+  override def main(args: Array[String]): Unit = {
     val parser = new scopt.OptionParser[Config]("java -jar genometools.jar snpmatrix [options]") {
       opt[File]('i', "input") required () action { (x, c) => c.copy(input = x) } text ("Multi-fasta file that's also input for creating phylogenetic tree. ")
       opt[File]('o', "output") action { (x, c) => c.copy(output = x) } text ("Output file. Default: snp.matrix.txt")
@@ -21,7 +23,7 @@ object PairwiseSNPdistance  extends Main{
 
     parser.parse(args, Config()).map { config =>
       run(config)
-    
+
     }
 
   }
@@ -29,30 +31,30 @@ object PairwiseSNPdistance  extends Main{
     FastaParser.forceEntries = true
     val es = new FileSource(config.input).read()
     val pw = new PrintWriter(config.output)
-    val pw2 = new PrintWriter(config.output+".comparable.tsv")
+    val pw2 = new PrintWriter(config.output + ".comparable.tsv")
     val list = es.iterator().toList.map(e => e.getID() -> e.sequence().stringRepresentation())
     println("Parsed input file")
     val sorted = list.sortBy(_._1)
     pw.print("Taxon\t" + sorted.map(_._1).mkString("\t") + "\n")
     pw2.print("Taxon\t" + sorted.map(_._1).mkString("\t") + "\n")
-    var counter=1
-    val ts=timestamp
-    val start=System.currentTimeMillis()
+    var counter = 1
+    val ts = timestamp
+    val start = System.currentTimeMillis()
     for (a <- list) {
-      println("Processing: "+a._1 +"\t"+counter+"/"+list.size+"\t"+new TimeInterval(System.currentTimeMillis() - start))
-      counter+=1
+      println("Processing: " + a._1 + "\t" + counter + "/" + list.size + "\t" + new TimeInterval(System.currentTimeMillis() - start))
+      counter += 1
       pw.print(a._1)
       pw2.print(a._1)
       for (b <- list) {
-        
+
         val paired = a._2.zip(b._2).count(f => f._1 != f._2 && f._1 != 'N' && f._2 != 'N')
-        val comparable= a._2.zip(b._2).count(f => f._1 != 'N' && f._2 != 'N')
+        val comparable = a._2.zip(b._2).count(f => f._1 != 'N' && f._2 != 'N')
         pw.print("\t" + paired)
-        pw2.print("\t"+comparable)
+        pw2.print("\t" + comparable)
 
       }
       pw.print("\n")
-       pw2.print("\n")
+      pw2.print("\n")
     }
     pw.close
     pw2.close
